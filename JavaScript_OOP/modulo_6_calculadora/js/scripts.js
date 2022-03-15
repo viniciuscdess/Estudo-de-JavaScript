7b1850fb2226438bbfa228b731247deb0b4e9473
Vinicius Carvalho <35708132+viniciuscdess@users.noreply.github.com>
2022-03-15 13:59:12 -0300

    curso de js poo 15/03
    comecando o javascript

--- JavaScript_OOP/modulo_6_calculadora/js/scripts.js
+++ JavaScript_OOP/modulo_6_calculadora/js/scripts.js
@@ -1,1 +1,26 @@
-console.log("Hello world!");
+class Calculator {
+
+	constructor() {
+		this.upperValue = document.querySelector("#upper-number"); //pegando os valores
+		this.resultValue = document.querySelector("#result-number");
+		this.reset = 0;
+	}
+
+	btnPress() {
+		console.log('apertou');
+	}
+}
+
+let calc = new Calculator();
+
+console.log(calc);
+
+//start btns
+let buttons = document.querySelectorAll('.btn');
+
+console.log(buttons);
+
+//map all butons
+for(let i = 0; buttons.length > i; i++) {
+	buttons[i].addEventListener('click', calc.btnPress);
+}

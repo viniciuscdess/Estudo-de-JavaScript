056314273be013ecbba332b1907f37035ef15509
Vinicius Carvalho <35708132+viniciuscdess@users.noreply.github.com>
2022-03-22 11:12:30 -0300

    curso de js poo 22/03
    finalizando curso de javascript orientado a objeto

--- JavaScript_OOP/ultimo-modulo-10-toDoList/js/scripts.js
+++ JavaScript_OOP/ultimo-modulo-10-toDoList/js/scripts.js
@@ -17,6 +17,65 @@ class ToDo {
 		let list = document.querySelector("#tasks-container");
 		// inserir na lista
 		list.appendChild(template); // (appendChild) -> método que insere um html em outro
+
+		// adiciona evento as tasks
+		this.addEvents();
+
+		this.checkTasks("add");
+	}
+
+	// ver a ultima task adicionada e adicionar dois eventos
+	addEvents() {
+		let removeBtns = document.querySelectorAll(".fa-trash");
+		let removeBtn = removeBtns[removeBtns.length - 1]; // acesso ao ultimo botão de remover(lixeira) da lista
+
+		// adiciona evento de remover
+		removeBtn.addEventListener("click", function() {
+			toDo.removeTask(this); // this se refere ao elemento clicado
+		});
+
+		let doneBtns = document.querySelectorAll(".fa-check");
+		let doneBtn = doneBtns[doneBtns.length - 1]; // acesso ao ultimo botão de concluido(check) da lista
+
+		// adiciona evento de completar tarefa
+		doneBtn.addEventListener("click", function() {
+			toDo.completeTask(this);
+		});
+	}
+
+	// função que remove a task
+	removeTask(task) {
+		// achar o elemento pai (div task)
+		let parentEl = task.parentElement;
+
+		// remove da lista (remove a div(uma tarefa) inteira)
+		parentEl.remove();
+
+		this.checkTasks("remove");
+	}
+
+	completeTask(task) {
+		// adiciona a clase de done
+		task.classList.add("done");
+	}
+
+	// verifica se tem alguma task, se tiver remove a mensagem(ñ possui tasks)
+	checkTasks(command) {
+		let msg = document.querySelector("#empty-tasks");
+
+		// logica para adicionar ou remover tasks
+		if(command === "add") {
+			this.total += 1;
+		} else if(command === "remove") {
+			this.total -= 1;
+		}
+
+		//checa se tem mais de uma task e adiciona ou remove a classe
+		if(this.total == 1) {
+			msg.classList.remove("hide");
+		} else {
+			msg.classList.add("hide");
+		}
 	}
 }
 
